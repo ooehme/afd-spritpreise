0e50eb583820ee5fa7af11f06e128806ef4ffaeb
Oliver Oehme <oliver.oehme@gmail.com>
2026-08-19 18:20:04 +0200

    Remove obsolete container block registrations

--- includes/class-block.php
+++ includes/class-block.php
@@ -6,8 +6,11 @@ defined('ABSPATH') || exit;
 
 final class Block
 {
+    private readonly ComponentRenderer $renderer;
+
     public function __construct(private readonly Plugin $plugin)
     {
+        $this->renderer = new ComponentRenderer($plugin);
     }
 
     public function register_hooks(): void
@@ -18,32 +21,47 @@ final class Block
 
     public function register(): void
     {
-        register_block_type(AFDSP_DIR . 'block', ['render_callback' => [$this, 'render']]);
-    }
-
-    public function render(array $attributes): string
-    {
-        return sprintf(
-            '<div %s>%s</div>',
-            get_block_wrapper_attributes(),
-            $this->plugin->render($attributes)
-        );
+        register_block_type(AFDSP_DIR . 'block', [
+            'render_callback' => [$this->renderer, 'parent'],
+        ]);
+        register_block_type(AFDSP_DIR . 'block/header', [
+            'render_callback' => [$this->renderer, 'header'],
+        ]);
+        register_block_type(AFDSP_DIR . 'block/fuel-tabs', [
+            'render_callback' => [$this->renderer, 'tabs'],
+        ]);
+        register_block_type(AFDSP_DIR . 'block/metric', [
+            'render_callback' => [$this->renderer, 'metric'],
+        ]);
+        register_block_type(AFDSP_DIR . 'block/tank-saving', [
+            'render_callback' => [$this->renderer, 'tank_saving'],
+        ]);
+        register_block_type(AFDSP_DIR . 'block/cheapest-station', [
+            'render_callback' => [$this->renderer, 'station'],
+        ]);
+        register_block_type(AFDSP_DIR . 'block/demands', [
+            'render_callback' => [$this->renderer, 'demands'],
+        ]);
+        register_block_type(AFDSP_DIR . 'block/method', [
+            'render_callback' => [$this->renderer, 'method'],
+        ]);
     }
 
     public function editor_config(): void
     {
         $options = Options::get();
+
         wp_localize_script('afdsp-area-picker', 'afdspAreaPickerConfig', [
             'endpoint' => esc_url_raw(rest_url('afd-spritpreise/v1/photon')),
             'nonce' => wp_create_nonce('wp_rest'),
             'area' => $options['area'],
-            'display' => $options['display'],
             'strings' => [
                 'search' => __('Ort oder PLZ suchen', 'afd-spritpreise'),
                 'noResults' => __('Keine Orte gefunden.', 'afd-spritpreise'),
                 'error' => __('Ortssuche derzeit nicht verfügbar.', 'afd-spritpreise'),
             ],
         ]);
+
         wp_enqueue_style('afdsp-admin');
     }
 }
