aa8ca4812a5ca46656ddc5ea9379fb93a3a19073
Oliver Oehme <oliver.oehme@gmail.com>
2026-08-21 17:15:57 +0200

    Register shared Gutenberg block icon script

--- includes/class-assets.php
+++ includes/class-assets.php
@@ -23,10 +23,11 @@ final class Assets
         wp_register_style('afdsp-admin', AFDSP_URL . 'assets/css/admin.css', [], AFDSP_VERSION);
         wp_register_script('afdsp-area-picker', AFDSP_URL . 'assets/js/area-picker.js', ['wp-api-fetch'], AFDSP_VERSION, true);
         wp_register_script('afdsp-admin-shortcodes', AFDSP_URL . 'assets/js/admin-shortcodes.js', [], AFDSP_VERSION, true);
+        wp_register_script('afdsp-block-icons', AFDSP_URL . 'assets/js/block-icon.js', ['wp-element', 'wp-hooks'], AFDSP_VERSION, true);
         wp_register_script(
             'afdsp-block-editor',
             AFDSP_URL . 'assets/js/block.js',
-            ['wp-blocks', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render', 'afdsp-area-picker'],
+            ['wp-blocks', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render', 'afdsp-area-picker', 'afdsp-block-icons'],
             AFDSP_VERSION,
             true
         );
